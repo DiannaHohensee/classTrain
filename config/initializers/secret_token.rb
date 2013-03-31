b84b2b169d273eede6aceca856e3dcf80abb2978
Dianna Hohensee <Dianna@dyn-209-2-211-53.dyn.columbia.edu>
2013-03-31 18:15:05 -0400

    basic template for classTrain

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassTrain::Application.config.secret_token = '0aedfafb2487bec64985455174c8646b251f1d8ade84b351646412f87a690627b0b75e20119120a91c53ba2090bf799122f0ddc7ba9df0402c7996187bdcdb83'
